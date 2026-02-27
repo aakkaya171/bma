c373817dd6b64b06aefa49ce159af18be317c812
aakkaya171 <aakkaya171@gmail.com>
2026-02-27 23:47:28 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+window.STATIONS = {
+  "Vorgartenstraße U1": {
+    images: [
+      "images/vorgarten_n+1.jpg",
+      "images/vorgarten_n0.jpg",
+      "images/vorgarten_n-1.jpg",
+      "images/vorgarten_n-2.jpg"
+    ]
+  }
+};
