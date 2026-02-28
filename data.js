85badeed7b432d15761dc58d2c01b54337756e3f
aakkaya171 <aakkaya171@gmail.com>
2026-02-28 11:44:09 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,41 +1,22 @@
 // data.js
 // Struktur:
-// STATIONS[stationKey].levels[levelKey] = [
-//   { src: "imagePath", marks: [{x,y}, ...] }
-// ]
+// STATIONS[stationName].levels[levelKey] = [ { src: "images/...", marks: [] }, ... ]
 
 window.STATIONS = {
   "Vorgartenstraße U1": {
     levels: {
-
       "N+1": [
-        {
-          src: "images/vorgarten_n+1.jpg",
-          marks: []
-        }
+        { src: "images/vorgarten_n+1.jpg", marks: [] }
       ],
-
       "N0": [
-        {
-          src: "images/vorgarten_n0.jpg",
-          marks: []
-        }
+        { src: "images/vorgarten_n0.jpg", marks: [] }
       ],
-
       "N-1": [
-        {
-          src: "images/vorgarten_n-1.jpg",
-          marks: []
-        }
+        { src: "images/vorgarten_n-1.jpg", marks: [] }
       ],
-
       "N-2": [
-        {
-          src: "images/vorgarten_n-2.jpg",
-          marks: []
-        }
+        { src: "images/vorgarten_n-2.jpg", marks: [] }
       ]
-
     }
   }
 };
